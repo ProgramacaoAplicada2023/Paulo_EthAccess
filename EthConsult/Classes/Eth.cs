bdc058709ca508866fb7719bacd98377139ef7d1
JoaoPauloIME <147888882+JoaoPauloIME@users.noreply.github.com>
2023-10-29 18:13:52 -0300

    Importing VS Project

---
+++ EthConsult/Classes/Eth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthConsult.Classes
+{
+    internal class Eth
+    {
+        private string infuraKey = "4dc468ed76354f90b1de9c69d9f85dac";
+        
+    }
+}
---
+++ EthConsult/Classes/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthConsult.Classes
+{
+    internal class User
+    {
+        private string login { get; set; }
+        private string password { get; set; }
+        private List<string> wallets { get; set; }
+
+        
+    }
+}
